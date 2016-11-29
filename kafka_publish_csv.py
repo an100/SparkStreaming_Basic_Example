3beb4088dae116af2d6681bcf76838bff055cccc
Dan Zaratsian <dtz001@gmail.com>
2016-11-28 22:54:41 -0500

    Update kafka_publish_csv.py

--- kafka_publish_csv.py
+++ kafka_publish_csv.py
@@ -4,20 +4,22 @@ import datetime,time
 import random
 from kafka import KafkaProducer
 
-data_file   = '/tmp/mosaic_data.csv' 
+try:
+    number_of_records = int(sys.argv[1])
+except:
+    number_of_records = 10
+
 brokers     = ['sandbox.hortonworks.com:6667']
 topic       = 'dztopic1'
-
-file    = csv.reader(open(data_file, 'rb'))
-header  = file.next()
 
 #producer = KafkaProducer(bootstrap_servers=brokers, value_serializer=lambda m: json.dumps(m).encode('ascii'))
 producer = KafkaProducer(bootstrap_servers=brokers)
 
-for row in file:
-    time.sleep(4)
+for i in range(number_of_records):
+    row = [str(random.randint(1000000,9999999)), str(random.randint(0,1)), "10", "20", str(random.uniform(10.0, 90.0))]
     record = '|'.join(row)
     print str(record)
     producer.send(topic, record)
+    time.sleep(1)
 
 #ZEND
